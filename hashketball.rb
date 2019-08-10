3bbd3095c3493cf48ce159dfbae1b91f84f23ea1
John Guest <jcguest90@gmail.com>
2019-08-10 06:01:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -152,3 +152,11 @@ def team_names
     
   end
 end   
+
+def player_numbers(team_name)
+game_hash.map do |place, team|
+  if team[:team_name] == team_name 
+    return team[:colors]
+    end
+  end 
+end 
