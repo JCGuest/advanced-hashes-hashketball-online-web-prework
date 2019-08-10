8bc15809bc18ef38588980b0a23aa950da2e54d0
John Guest <jcguest90@gmail.com>
2019-08-10 06:38:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,13 +154,13 @@ def team_names
 end   
 
 def player_numbers(team_name)
-  #num = []
+  num = []
   game_hash.each do |place, team|
     if team[:team_name] == team_name
       team.each do |attribute, data|
         if attribute == :players
-          data.map do |player|
-            player[:number]
+          data.each do |player|
+            player[:number] << num
             
           end
         end     
