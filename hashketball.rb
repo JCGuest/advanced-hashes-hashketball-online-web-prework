a148a106198e1e69696a99c88d6ec52a435d07d5
John Guest <jcguest90@gmail.com>
2019-08-10 06:18:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -159,6 +159,7 @@ def player_numbers(team_name)
       team.each do |attribute, data|
         if attribute == :players
           data.each do |player|
+            player[:number]
           end
         end     
       end     
